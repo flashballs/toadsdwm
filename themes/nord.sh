5895f1c6a8c098c752b0f8edac752b35af6bba85
ZzqMa <buttlaface@gmail.com>
2026-04-14 15:38:01 -0500

    Fix paths for dwm and st configuration files

--- themes/nord.sh
+++ themes/nord.sh
@@ -1,4 +1,4 @@
-cp -f ~/toadsdwm/themes/nord/dwm ~/dwm/config.h &&
-cp -f ~/toadsdwm/themes/nord/st ~/st/config.h &&
+cp -f ~/toadsdwm/themes/nord/dwm ~/toadsdwm/dwm/config.h &&
+cp -f ~/toadsdwm/themes/nord/st ~/toadsdwm/st/config.h &&
 cp -f ~/toadsdwm/themes/nord/xinitrc ~/.xinitrc &&
 cp -f ~/toadsdwm/themes/nord/picom ~/.config/picom/picom.conf

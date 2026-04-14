4327683682c47d0571ef633a9179421ed7d20feb
ZzqMa <buttlaface@gmail.com>
2026-04-13 21:33:55 -0500

    Add files via upload

---
+++ themes/nord.sh
@@ -0,0 +1,4 @@
+cp -f ~/toadsdwm/themes/nord/dwm ~/dwm/config.h &&
+cp -f ~/toadsdwm/themes/nord/st ~/st/config.h &&
+cp -f ~/toadsdwm/themes/nord/xinitrc ~/.xinitrc &&
+cp -f ~/toadsdwm/themes/nord/picom ~/.config/picom/picom.conf
---
+++ themes/solarizeddark.sh
@@ -0,0 +1,5 @@
+cp -f ~/toadsdwm/themes/solarizeddark/dwm ~/dwm/config.h &&
+cp -f ~/toadsdwm/themes/solarizeddark/st ~/st/config.h &&
+cp -f ~/toadsdwm/themes/solarizeddark/xinitrc ~/.xinitrc &&
+cp -f ~/toadsdwm/themes/solarizeddark/picom ~/.config/picom/picom.conf
+

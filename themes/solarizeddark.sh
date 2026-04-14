c1afc62f9651051b9f401a6a2cb8fe1ab55c5699
ZzqMa <buttlaface@gmail.com>
2026-04-14 15:39:01 -0500

    Update paths for dwm and st configuration files

--- themes/solarizeddark.sh
+++ themes/solarizeddark.sh
@@ -1,5 +1,5 @@
-cp -f ~/toadsdwm/themes/solarizeddark/dwm ~/dwm/config.h &&
-cp -f ~/toadsdwm/themes/solarizeddark/st ~/st/config.h &&
+cp -f ~/toadsdwm/themes/solarizeddark/dwm ~/toadsdwm/dwm/config.h &&
+cp -f ~/toadsdwm/themes/solarizeddark/st ~/toadsdwm/st/config.h &&
 cp -f ~/toadsdwm/themes/solarizeddark/xinitrc ~/.xinitrc &&
 cp -f ~/toadsdwm/themes/solarizeddark/picom ~/.config/picom/picom.conf
 

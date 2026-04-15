9274f5c58b6ec56172061a4f8a8894165277eaed
ZzqMa <buttlaface@gmail.com>
2026-04-14 22:26:48 -0500

    Add files via upload

---
+++ themes/gruvbox.sh
@@ -0,0 +1,4 @@
+cp -f ~/toadsdwm/themes/gruvbox/dwm ~/toadsdwm/dwm/config.h &&
+cp -f ~/toadsdwm/themes/gruvbox/st ~/toadsdwm/st/config.h &&
+cp -f ~/toadsdwm/themes/gruvbox/xinitrc ~/.xinitrc &&
+cp -f ~/toadsdwm/themes/gruvbox/picom ~/.config/picom/picom.conf

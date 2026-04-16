a624177d2bdf39dc879125c719b21fd5760c70da
ZzqMa <buttlaface@gmail.com>
2026-04-15 22:27:15 -0500

    Add files via upload

---
+++ themes/catpuccin.sh
@@ -0,0 +1,4 @@
+cp -f ~/toadsdwm/themes/catpuccin/dwm ~/toadsdwm/dwm/config.h &&
+cp -f ~/toadsdwm/themes/catpuccin/st ~/toadsdwm/st/config.h &&
+cp -f ~/toadsdwm/themes/catpuccin/xinitrc ~/.xinitrc &&
+cp -f ~/toadsdwm/themes/catpuccin/picom ~/.config/picom/picom.conf
